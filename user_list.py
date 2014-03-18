058bbd0d9a22fbc20f478f3a594da13aa223d2dd
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-03-18 21:53:58 +0200

    Add merge function and test

--- user_list.py
+++ user_list.py
@@ -2,3 +2,11 @@ class User_list():
     def __init__(self, list_name):
         self.list_name = list_name
         self.list_of_users = []
+
+    def merge(self, llist):
+        new_list = []
+        for user in self.list_of_users:
+            new_list.append(user)
+        for user in llist:
+            new_list.append(user)
+        return new_list
--- user_list_tests.py
+++ user_list_tests.py
@@ -1,13 +1,24 @@
 import unittest
+from user import User
 from user_list import User_list
 
 class Test_user_list(unittest.TestCase):
     def setUp(self):
         self.new_user_list=User_list("new list 1")
+        for i in range(5):
+            user = User("user" + str(i), "user" + str(i) + "@mail.bg")
+            self.new_user_list.list_of_users.append(user)
+        self.new_user_list2=User_list("new list 2")
+        for i in range(5):
+            user = User("usr" + str(i), "user" + str(i) + "@mail.bg")
+            self.new_user_list2.list_of_users.append(user)
+
 
     def test_new_user_list(self):
-
         self.assertEqual("new list 1",self.new_user_list.list_name)
+
+    def test_merge(self):
+        self.assertEqual(self.new_user_list.merge(self.new_user_list2.list_of_users), self.new_user_list.list_of_users + self.new_user_list2.list_of_users)
 
 if __name__ == '__main__':
     unittest.main()
